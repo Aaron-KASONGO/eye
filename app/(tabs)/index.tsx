8f7ae914dea3afb119f2ab1ec88619e0b2b269ea
Aaron KASONGO <19kk181@esisalama.org>
2025-02-22 18:27:47 +0100

    all things

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,97 @@
-import { StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Entypo } from '@expo/vector-icons';
+import { useCameraPermissions } from 'expo-camera';
+import * as DocumentPicker from 'expo-document-picker';
+import { FloatingAction } from 'react-native-floating-action';
+import { router } from 'expo-router';
+import { CardImage } from '@/components/card-image/CardImage';
+
+const actions = [
+  {
+    text: "Photo",
+    icon: <Entypo name='camera' color={'white'} size={20} />,
+    name: "bt_photo",
+    position: 2
+  },
+  {
+    text: "Charger",
+    icon: <Entypo name='upload' color={'white'} size={20} />,
+    name: "bt_charger",
+    position: 1
+  }
+];
 
 export default function TabOneScreen() {
+  const [permission, requestPermission] = useCameraPermissions();
+
+  const pickImage = async () => {
+    const response = await DocumentPicker.getDocumentAsync({type: 'image/*'});
+    console.log(response);
+  }
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+    <ScrollView
+
+    >
+        <View
+          style={{
+            padding: 10,
+            rowGap: 10,
+          }}
+        >
+          <Text
+            style={styles.sectionText}
+          >Historique</Text>
+          <View
+            style={styles.listView}
+          >
+            {/* <CardImage />
+            <CardImage /> */}
+            
+          </View>
+        </View>
+    </ScrollView>
+    
+      <FloatingAction
+        actions={actions}
+        onPressItem={name => {
+          switch (name) {
+            case 'bt_photo':
+              if (permission) {
+                if (permission.granted) {
+                  router.navigate('/float-action/camera', {relativeToDirectory: true})
+                } else {
+                  requestPermission();
+                }
+              }
+              break;
+            
+            case 'bt_charger':
+              pickImage();
+              break;
+          
+            default:
+              break;
+          }
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  listView: {
+    gap: 10
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+  sectionText: {
+    fontSize: 15,
+    fontWeight: 'bold'
+  }
+})
